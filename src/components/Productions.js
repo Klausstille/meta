f8bf54996cef25be8171a1fb7bbde409fc90dffc
KLAUS STILLE <mail@klausstille.com>
2022-09-07 22:45:51 +0200

    bug fix

--- src/components/Productions.js
+++ src/components/Productions.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./Productions.css";
 // import ControlledCarousel from "./helpers/Carousel";
 import ProductionItem from "./helpers/IndexItem";
 import "./helpers/Index.css";
